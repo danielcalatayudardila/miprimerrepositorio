53ec1db9ffadf3442b691b29634c0e2f84eb6575
danielcalatayudardila <daniel.calatayud@plazacastilla.salesianas.org>
2025-04-24 12:15:42 +0200

    Add files via upload

---
+++ Dan.java
@@ -0,0 +1,10 @@
+package Dani;
+
+public class Dan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HOLA ESTE ES MI PRIMER PROGRAMA, ME PRESENTO SOY DANI, BUENAS");
+	}
+
+}
